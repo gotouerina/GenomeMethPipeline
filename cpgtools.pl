288b772b9ca9a7be0cca3f70c37e741c3ec7079f
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2024-02-27 19:57:50 +0800

    Update cpgtools.pl

--- cpgtools.pl
+++ cpgtools.pl
@@ -7,7 +7,7 @@ my $ref = shift;
 my $bam = shift;
 my $prefix = shift;
 
-my $dir = `pwd`
+my $dir = `pwd`;
 chomp $dir;
 open O, ">meth.sh" or die "Usage: perl $0 $ref $bam $prefix";
 print O "$dir/pbmm2 align $ref $bam $prefix.sort.bam --sort\n$dir/aligned_bam_to_cpg_scores --bam $prefix.sort.bam --output-prefix $prefix  --model $dir/pileup_calling_model.v1.tflite  --threads 10";
